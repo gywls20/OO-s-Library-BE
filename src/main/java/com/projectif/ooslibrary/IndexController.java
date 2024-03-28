d0ef591183dc87742bdb140253127c0df83d4289
leegyuchan <hoo788@gmail.com>
2024-03-28 10:42:45 +0900

    indexController model test - 이규찬

--- src/main/java/com/projectif/ooslibrary/IndexController.java
+++ src/main/java/com/projectif/ooslibrary/IndexController.java
@@ -1,13 +1,15 @@
 package com.projectif.ooslibrary;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("test", "test입니다!!");
         return "index/index";
     }
 }
