29b9630dce83f9d80ea8f58c8e84b47732f8dd32
leegyuchan <hoo788@gmail.com>
2024-04-04 10:40:27 +0900

    < fix >: nid.naver

--- src/main/java/com/projectif/ooslibrary/config/WebMvcConfig.java
+++ src/main/java/com/projectif/ooslibrary/config/WebMvcConfig.java
@@ -13,7 +13,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://192.168.0.8:3000", "https://libraryif.vercel.app:3000")
+                .allowedOrigins("http://localhost:3000", "http://192.168.0.8:3000", "https://libraryif.vercel.app:3000",
+                        "https://nid.naver.com")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods(HttpMethod.OPTIONS.name(), HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name());
