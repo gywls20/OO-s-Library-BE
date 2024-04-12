80669dc2251b3952099fbb5c2b7d7f91c03c1a55
Tofubeen <qornjsgur1215@naver.com>
2024-04-12 13:07:11 +0900

    feat: admin페이지 추가
     - 회원목록 구현
     -
     -

---
+++ src/main/java/com/projectif/ooslibrary/admin/service/AdMemberService.java
@@ -0,0 +1,10 @@
+package com.projectif.ooslibrary.admin.service;
+
+import com.projectif.ooslibrary.admin.domain.MemberVO;
+
+import java.util.List;
+
+public interface AdMemberService {
+
+    List<MemberVO> findAllMember();
+}
