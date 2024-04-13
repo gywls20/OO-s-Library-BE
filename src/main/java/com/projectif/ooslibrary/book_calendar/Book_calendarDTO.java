e9a04648d7edf98d1f012127a60c008223d17f03
gywls20 <koj858@naver.com>
2024-04-14 01:32:50 +0900

    feat: 읽은책 이력 관련 정보 가져와 res

--- src/main/java/com/projectif/ooslibrary/book_calendar/Book_calendarDTO.java
+++ src/main/java/com/projectif/ooslibrary/book_calendar/Book_calendarDTO.java
@@ -6,15 +6,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class Book_calendarDTO {
-    private Long bookCalendarPk;
-    private int bookCalendarReadPercent;
-    private MyLibrary myLibrary;
-    private String bookCalendarScheduleDate;
-    private BookPlus bookPlus;
 
+    private String bookTitle;
+    private String coverImagePath;
+
+    private int bookCalendarReadPercent;
+    private LocalDateTime bookCalendarScheduleDate;
+
+    //이건 왜 있어야하는거지?
+    public Book_calendarDTO(String bookTitle, String coverImagePath, int bookCalendarReadPercent, LocalDateTime bookCalendarScheduleDate) {
+        this.bookTitle = bookTitle;
+        this.coverImagePath = coverImagePath;
+        this.bookCalendarReadPercent = bookCalendarReadPercent;
+        this.bookCalendarScheduleDate = bookCalendarScheduleDate;
+    }
 
 }
