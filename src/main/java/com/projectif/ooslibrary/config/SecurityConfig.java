8197abadab9d046cace277d35cc0b9cb7848dea4
gywls20 <koj858@naver.com>
2024-04-14 21:59:18 +0900

    feat: library 페이지
    - 책 추가 기능
    - 책 전체 리스트업

--- src/main/java/com/projectif/ooslibrary/config/SecurityConfig.java
+++ src/main/java/com/projectif/ooslibrary/config/SecurityConfig.java
@@ -133,6 +133,7 @@ hu       * AccessDeniedHandler : 권한(인가) 예외처리, 403(Forbidden) 상
                                 .requestMatchers("/comment/**").permitAll()
                                 .requestMatchers("/admin/**", "/api/v1/**").hasRole(Role.ADMIN.name())
                                 .requestMatchers("/readData/**").permitAll()
+                                .requestMatchers("/library/**").permitAll()
                                 .anyRequest().authenticated() 
                 )
                 // 세션 관리 기능
