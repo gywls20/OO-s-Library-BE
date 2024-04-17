5ff56e04b1ba996df26576387234807be82e4a76
gywls20 <koj858@naver.com>
2024-04-17 10:38:20 +0900

    feat: 북캘린더 추가

--- src/main/java/com/projectif/ooslibrary/book_calendar/controller/Book_calendarController.java
+++ src/main/java/com/projectif/ooslibrary/book_calendar/controller/Book_calendarController.java
@@ -1,13 +1,17 @@
 package com.projectif.ooslibrary.book_calendar.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projectif.ooslibrary.book_calendar.Book_calendarDTO;
 import com.projectif.ooslibrary.book_calendar.service.Book_calendarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @Controller
@@ -15,7 +19,8 @@ import java.util.List;
 public class Book_calendarController {
 
     private final Book_calendarService bookCalendarService;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @PostMapping("/readData")
     @ResponseBody
     public ResponseEntity<String> saveBookData(@RequestParam int percent,
@@ -31,4 +36,19 @@ public class Book_calendarController {
         return bookCalendarService.getBookCalendar(myLibrary);
     }
 
+    // 북캘린더 페이지 이동
+    @GetMapping("/calendar/{myLibrary}")
+    public String bookCalendar(@PathVariable Long myLibrary , Model model){
+
+        List<Book_calendarDTO> bookCalendars = bookCalendarService.getBookCalendar(myLibrary);
+
+        try {
+            String bookCalendarsJson = objectMapper.writeValueAsString(bookCalendars);
+
+            model.addAttribute("bookCalendarListJson", bookCalendarsJson);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "library/bookCalendar";
+    }
 }
